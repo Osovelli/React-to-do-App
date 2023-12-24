c7ef705c497a6c0894858701e226708fac4f38cf
Sithabo <sithabisoh@muwci.net>
2023-12-24 15:09:21 +0100

    disabled flowbite plugin from tailwindcss.config

--- views/tailwind.config.js
+++ views/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 }
